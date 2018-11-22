c7ff5d3d83c7488402476cf8a1f13e8b3f8fdf59
Bjorn Rogeberg <bjorn.rogeberg@basefarm.com>
2018-11-22 11:03:21 +0100

    Increased version number

--- session-tool.sh
+++ session-tool.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SESSION_TOOL_VERSION=1.5.1
+SESSION_TOOL_VERSION=1.5.2
 PUBURL="https://raw.githubusercontent.com/basefarm/aws-session-tool/master/session-tool.sh"
 #
 # Bash utility to
